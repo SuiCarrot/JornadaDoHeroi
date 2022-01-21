cd1332261cbebed6829e2fb7f00b8a466f7deb87
SuiCarrot <brunoh.luka@gmail.com>
2022-01-20 22:21:35 -0300

    Primeiro up

---
+++ jornadadoheroi.js
@@ -0,0 +1,21 @@
+// DATA DE ENTREGA DIA 30/01 DOMINGO
+
+
+var prompt = require("prompt-sync")();
+
+console.log(
+  "Após longas semanas viajando você avista os portões da grande cidade de Delendill, o cheiro de água salgada que já lhe acompanhava nos ultimos dias se mistura ao cheiro de peixes e suor. O som das ondas quebrando no porto é superado apenas pelos gritos dos vendedores que se acumulam já na entrada da cidade, gritando a plenos pulmões suas ofertas na tentativa de conseguirem vender suas mercadorias."
+);
+console.log(
+  "Ao se aproximar dos portões da cidade os guardas lhe param, afinal um viajante portando armas ou é um aventureiro ou um bandido, e ambos geralmente causam problemas. Um homem de pele já curtida pelo sol lhe pergunta com voz grave:"
+);
+const nome = prompt("-Qual o seu nome, viajante? ");
+
+console.log(nome);
+
+//var count = 0;
+//function contador() {
+//  count++;
+//}
+
+
